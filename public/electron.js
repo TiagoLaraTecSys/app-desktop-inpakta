594b0eb981ca067f9d75c4060378b901cfc30197
tiago de lara ribeiro <laratecsys@gmail.com>
2020-07-30 22:47:32 -0300

    First commit settingUp the projetc with React and Electron

---
+++ public/electron.js
@@ -0,0 +1,29 @@
+const { BrowserWindow, app } = require('electron')
+const path = require("path")
+const isDev = require("electron-is-dev")
+
+function createWindow() {
+    //Criando Jnela de navegação
+
+    let win = new BrowserWindow({
+        widh: 1024,
+        height: 780,
+        webPreferences: {
+            nodeIntegration: false;
+        }
+    });
+
+    win.loadURL(
+        isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`
+
+    );
+
+    win.whenReady().then(createWindow)
+
+    app.on('activate', () => {
+
+        if (BrowserWindow.getAllWindows().length === 0){
+            createWindow();
+        }
+    })
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+		<header>
+			<h1>pega na minha bola</h1>
+			<nav>
+				<ul>
+					<li>
+						<a href="/">Menu1</a>
+					</li>
+					<li>
+						<a href="/">Menu2</a>
+					</li>
+					<li>
+						<a href="/">Menu3</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+  );
+}
+
+export default Header;
