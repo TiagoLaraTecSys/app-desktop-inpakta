3d22515afd9cefaaa21e7d71aee4fd6b83a13a89
rafael <rafael@bluefoot.com.br>
2020-08-26 22:35:31 -0300

    novo login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,36 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
 import {reduxForm, Field} from 'redux-form';
 import {submitUserAction} from '../../services/UserServices/UserLogin'
 import { listAllDbProperties } from '../../services/UserServices/ListAllDbProperties';
 
-const UserFormFun = props =>{
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: this.defaultData,
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  defaultData = {
+    senha: '',
+		email: '',
+  };
 
-  const { handleSubmit } = props
+  handleChange(e) {
+    let { name, value } = e.target;
+    const { data } = this.state;
+    this.setState({data: {...data, [name]: value} });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const {
+      data,
+    } = this.state;
+
+    if (data !== '') {
+      
+      submitUserAction(data)
+        
+    } else {
+      alert("deu erraddo fdp")
+    }
+  }
   
-  const submit = (data, submitUserAction) => {submitUserAction(data)}
-  return (
-    <div className="inp-login">
-      <form action="/componentList" className="inp-login__form" onSubmit={handleSubmit((fields) => submit(fields, submitUserAction))}>
-          <label>Email</label>
-          <Field type="text" component="input" name="email"/>
-          <label>Senha</label>
-          <Field type="password" component="input" name="senha"/>
-          <button to="/componentList" className="inp-login__btn" type="submit">Submit</button>
-          <Link to="/cadastro" onClick={console.log('olá')}>ou se cadastre</Link>
+  render() {
+    const {
+      data: {
+        senha,
+        email
+      },
+    } = this.state;
+
+    return (
+      <div className="inp-login">
+       <form action="/componentList" className="inp-login__form" onSubmit={this.handleSubmit}>
+          <label>
+            Email
+          <input
+            type='email'
+            name='email'
+            placeholder="E-mail*"
+            onChange={this.handleChange}
+            className='bf-input__general'
+            value={email}
+            required
+          />
+          </label>
+          <label >
+            Senha
+            <input
+              type='password'
+              name='senha'
+              onChange={this.handleChange}
+              className='bf-input__general'
+              value={senha}
+              required
+            />
+          </label>
+           <button to="/componentList" className="inp-login__btn" type="submit">Submit</button>
+           <Link to="/cadastro" onClick={console.log('olá')}>ou se cadastre</Link>
       </form>
-    </div>
-  );
+     </div>
+    )
+  }
 }
+export default Login
 
-const UserForm = (reduxForm({
-  form: "formUser"
-}))(UserFormFun)
+// const UserFormFun = props =>{
 
-const mapStateToProps = state => ({
+//   const { handleSubmit } = props
+  
+//   const submit = (data, submitUserAction) => {submitUserAction(data)}
+//   return (
+//     <div className="inp-login">
+//       <form action="/componentList" className="inp-login__form" onSubmit={handleSubmit((fields) => submit(fields, submitUserAction))}>
+//           <label>Email</label>
+//           <Field type="text" component="input" name="email"/>
+//           <label>Senha</label>
+//           <Field type="password" component="input" name="senha"/>
+//           <button to="/componentList" className="inp-login__btn" type="submit">Submit</button>
+//           <Link to="/cadastro" onClick={console.log('olá')}>ou se cadastre</Link>
+//       </form>
+//     </div>
+//   );
+// }
+
+// const UserForm = (reduxForm({
+//   form: "formUser"
+// }))(UserFormFun)
+
+// const mapStateToProps = state => ({
 
 
-})
+// })
 
-export default connect(mapStateToProps, {submitUserAction})(UserForm)
+// export default connect(mapStateToProps, {submitUserAction})(UserForm)
