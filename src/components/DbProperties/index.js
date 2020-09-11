0132af2481c42c1e8bc160824b8b5d5b01b5bc4d
rafael <rafael@bluefoot.com.br>
2020-09-10 23:31:37 -0300

    tela dbProperties

--- src/components/DbProperties/index.js
+++ src/components/DbProperties/index.js
@@ -1,18 +1,153 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import {insertDbProperties} from '../../services/UserServices/DBServices/InsertDbProperties'
 
-class CadastroDbProperties extends Component {
-    constructor(){
-        super();
-    }
-    
-    render(){
 
-        return(
-            <div>
-                <h1>Hello Word é o caraio Fião</h1>
-            </div>
-        )
+class DbProperties extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: this.defaultData,
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  defaultData = {
+    repository: '',
+    vendor: '',
+    driver: '',
+    username: '',
+    password: '',
+    dbschema: '',
+    url: '',
+    isActive:'',
+    dataDiscoveryProperties_id:''
+  };
+
+  handleChange(e) {
+    let { name, value } = e.target;
+    const { data } = this.state;
+    this.setState({data: {...data, [name]: value} });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const {
+      data,
+    } = this.state;
+
+    if (data !== '') {
+      
+      insertDbProperties(data)
+        
+    } else {
+      alert("deu erraddo fdp")
+    }
+  }
+  
+  render() {
+    const {
+      data: {
+        vendor,
+        driver,
+        password,
+        username,
+        dbschema,
+        url
+      },
+    } = this.state;
+
+    return (
+      <div className="cadastro">
+        <form action="/componentList" className="cadastro__form" onSubmit={this.handleSubmit}>
+          <label>
+            Gerenciador
+          </label>
+          <select
+            type='text'
+            name='vendor'
+            onChange={this.handleChange}
+            className='cadastro__form--input'
+            value={vendor}
+            required
+          >
+            <option>Selecione</option>
+            <option>MySql</option>
+            <option>Oracle</option>
+          </select>
+          <label>
+            Driver
+          </label>
+          <input
+            type='text'
+            name='driver'
+            onChange={this.handleChange}
+            className='cadastro__form--input'
+            value={driver}
+            required
+          />
+          <label>
+            Usuário
+          </label>
+          <input
+            type='text'
+            name='username'
+            onChange={this.handleChange}
+            className='bf-input__general'
+            value={username}
+            required
+          />
+          
+          <label >
+            Senha
+          </label>
+          <input
+            type='text'
+            name='password'
+            onChange={this.handleChange}
+            className='bf-input__general'
+            value={password}
+            required
+          />
+          <label >
+            Data base
+          </label>
+          <input
+            type='text'
+            name='dbschema'
+            onChange={this.handleChange}
+            className='bf-input__general'
+            value={dbschema}
+            required
+          />
+
+          <label >
+            Url
+          </label>
+          <input
+            type='text'
+            name='url'
+            onChange={this.handleChange}
+            className='bf-input__general'
+            value={url}
+            required
+          />
+
+          <label >
+            <input
+              type="checkbox"
+              name='isActive'
+              className="check"
+              value="1"
+              onChange={this.handleChange}
+            />
+            Ativado
+          </label>
+          
+           <button to="/componentList" className="cadastro__btn" type="submit">Submit</button>
+         
+      </form>
+     </div>
+    )
+  }
 }
-
-export default CadastroDbProperties
+export default DbProperties
